4c336cedbfaf3dfa319f6e0543a264c4da141e32
Eric Larson <eclarson@smu.edu>
2020-08-28 09:51:14 -0500

    Play sine wave with slider. Examples given for both swift loop and c loop (with c loop being more efficient)

--- AudioLabSwift/AudioModel.swift
+++ AudioLabSwift/AudioModel.swift
@@ -13,32 +13,11 @@ class AudioModel {
     
     // MARK: Properties
     private var BUFFER_SIZE:Int
-    var timeData:[Float]
-    var fftData:[Float]
     
     // MARK: Public Methods
-    init(buffer_size:Int) {
-        BUFFER_SIZE = buffer_size
-        // anything not lazily instatntiated should be allocated here
-        timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)
-        fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
-    }
-    
-    // public function for starting processing of microphone data
-    func startMicrophoneProcessing(withFps:Double){
-        self.audioManager?.inputBlock = self.handleMicrophone
+    init() {
+        BUFFER_SIZE = 0
         
-        // repeat this fps times per second using the timer class
-        Timer.scheduledTimer(timeInterval: 1.0/withFps, target: self,
-                            selector: #selector(self.runEveryInterval),
-                            userInfo: nil,
-                            repeats: true)
-    }
-    
-    // public function for playing from a file reader file
-    func startProcesingAudioFileForPlayback(){
-        self.audioManager?.outputBlock = self.handleSpeakerQueryWithAudioFile
-        self.fileReader?.play()
     }
     
     func startProcessingSinewaveForPlayback(withFreq:Float=330.0){
@@ -55,25 +34,6 @@ class AudioModel {
         self.audioManager?.play()
     }
     
-    // Here is an example function for getting the maximum frequency
-    func getMaxFrequencyMagnitude() -> (Float,Float){
-        // this is the slow way of getting the maximum...
-        // you might look into the Accelerate framework to make things more efficient
-        var max:Float = -1000.0
-        var maxi:Int = 0
-        
-        if inputBuffer != nil {
-            for i in 0..<Int(fftData.count){
-                if(fftData[i]>max){
-                    max = fftData[i]
-                    maxi = i
-                }
-            }
-        }
-        let frequency = Float(maxi) / Float(BUFFER_SIZE) * Float(self.audioManager!.samplingRate)
-        return (max,frequency)
-    }
-    // for sliding max windows, you might be interested in the following: vDSP_vswmax
     
     //==========================================
     // MARK: Private Properties
@@ -81,90 +41,20 @@ class AudioModel {
         return Novocaine.audioManager()
     }()
     
-    private lazy var fftHelper:FFTHelper? = {
-        return FFTHelper.init(fftSize: Int32(BUFFER_SIZE))
-    }()
-    
-    private lazy var outputBuffer:CircularBuffer? = {
-        return CircularBuffer.init(numChannels: Int64(self.audioManager!.numOutputChannels),
-                                   andBufferSize: Int64(BUFFER_SIZE))
-    }()
-    
-    private lazy var inputBuffer:CircularBuffer? = {
-        return CircularBuffer.init(numChannels: Int64(self.audioManager!.numInputChannels),
-                                   andBufferSize: Int64(BUFFER_SIZE))
-    }()
-    
     
     //==========================================
     // MARK: Private Methods
-    private lazy var fileReader:AudioFileReader? = {
-        
-        if let url = Bundle.main.url(forResource: "satisfaction", withExtension: "mp3"){
-            var tmpFileReader:AudioFileReader? = AudioFileReader.init(audioFileURL: url,
-                                                   samplingRate: Float(audioManager!.samplingRate),
-                                                   numChannels: audioManager!.numOutputChannels)
-            
-            tmpFileReader!.currentTime = 0.0
-            print("Audio file succesfully loaded for \(url)")
-            return tmpFileReader
-        }else{
-            print("Could not initialize audio input file")
-            return nil
-        }
-    }()
+    
     
     //==========================================
     // MARK: Model Callback Methods
-    @objc
-    private func runEveryInterval(){
-        if inputBuffer != nil {
-            // copy data to swift array
-            self.inputBuffer!.fetchFreshData(&timeData, withNumSamples: Int64(BUFFER_SIZE))
-            
-            // now take FFT and display it
-            fftHelper!.performForwardFFT(withData: &timeData,
-                                         andCopydBMagnitudeToBuffer: &fftData)
-            
-            
-        }
-    }
-    
    
     
     //==========================================
     // MARK: Audiocard Callbacks
     // in obj-C it was (^InputBlock)(float *data, UInt32 numFrames, UInt32 numChannels)
     // and in swift this translates to:
-    private func handleMicrophone (data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32) {
-//        var max:Float = 0.0
-//        if let arrayData = data{
-//            for i in 0..<Int(numFrames){
-//                if(abs(arrayData[i])>max){
-//                    max = abs(arrayData[i])
-//                }
-//            }
-//        }
-//        // can this max operation be made faster??
-//        print(max)
-        
-        // copy samples from the microphone into circular buffer
-        self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
-    }
     
-    private func handleSpeakerQueryWithAudioFile(data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32){
-        if let file = self.fileReader{
-            
-            // read from file, loaidng into data (a float pointer)
-            file.retrieveFreshAudio(data,
-                                    numFrames: numFrames,
-                                    numChannels: numChannels)
-            
-            // set samples to output speaker buffer
-            self.outputBuffer?.addNewFloatData(data,
-                                         withNumSamples: Int64(numFrames))
-        }
-    }
     
     //    _     _     _     _     _     _     _     _     _     _
     //   / \   / \   / \   / \   / \   / \   / \   / \   / \   /
@@ -178,6 +68,9 @@ class AudioModel {
             self.audioManager?.sineFrequency = sineFrequency
         }
     }
+    
+    // everything below here is for the swift implementation
+    // this can be deleted when using the objective c implementation
     private var phase:Float = 0.0
     private var phaseIncrement:Float = 0.0
     private var sineWaveRepeatMax:Float = Float(2*Double.pi)
--- AudioLabSwift/ViewController.swift
+++ AudioLabSwift/ViewController.swift
@@ -10,61 +10,25 @@ import UIKit
 import Metal
 
 
-let AUDIO_BUFFER_SIZE = 1024*4
-
-
 class ViewController: UIViewController {
 
     
-    let audio = AudioModel(buffer_size: AUDIO_BUFFER_SIZE)
-    lazy var graph:MetalGraph? = {
-        return MetalGraph(mainView: self.view)
-    }()
-    
-    
+    let audio = AudioModel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // add in graphs for display
-        graph?.addGraph(withName: "fft",
-                        shouldNormalize: true,
-                        numPointsInGraph: AUDIO_BUFFER_SIZE/2)
-        
-        graph?.addGraph(withName: "time",
-            shouldNormalize: false,
-            numPointsInGraph: AUDIO_BUFFER_SIZE)
-        
-        // just start up the audio model here
-        audio.startMicrophoneProcessing(withFps: 10)
-        //audio.startProcesingAudioFileForPlayback()
-        audio.startProcessingSinewaveForPlayback(withFreq: 630.0)
+        audio.startProcessingSinewaveForPlayback(withFreq: 1000)
         audio.play()
-        
-        // run the loop for updating the graph peridocially
-        Timer.scheduledTimer(timeInterval: 0.05, target: self,
-            selector: #selector(self.updateGraph),
-            userInfo: nil,
-            repeats: true)
        
     }
     
     
-    @objc
-    func updateGraph(){
-        self.graph?.updateGraph(
-            data: self.audio.fftData,
-            forKey: "fft"
-        )
-        
-        self.graph?.updateGraph(
-            data: self.audio.timeData,
-            forKey: "time"
-        )
-        
+    @IBOutlet weak var freqLabel: UILabel!
+    @IBAction func changeFrequency(_ sender: UISlider) {
+        self.audio.sineFrequency = sender.value
+        freqLabel.text = "Frequency: \(sender.value)"
     }
     
-    
-
 }
 
