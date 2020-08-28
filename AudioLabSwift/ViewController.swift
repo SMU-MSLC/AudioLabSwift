c3da2a6905d10c6c9890e381b9857cc9c88f7ea4
Eric Larson <eclarson@smu.edu>
2020-08-28 10:04:31 -0500

    bare implementation of the audio card access and usage (no circular buffers used in order to make things very simple)

--- AudioLabSwift/ViewController.swift
+++ AudioLabSwift/ViewController.swift
@@ -10,58 +10,22 @@ import UIKit
 import Metal
 
 
-let AUDIO_BUFFER_SIZE = 1024*4
 
 
 class ViewController: UIViewController {
 
     
-    let audio = AudioModel(buffer_size: AUDIO_BUFFER_SIZE)
-    lazy var graph:MetalGraph? = {
-        return MetalGraph(mainView: self.view)
-    }()
-    
+    let audio = AudioModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        // add in graphs for display
-        graph?.addGraph(withName: "fft",
-                        shouldNormalize: true,
-                        numPointsInGraph: AUDIO_BUFFER_SIZE/2)
-        
-        graph?.addGraph(withName: "time",
-            shouldNormalize: false,
-            numPointsInGraph: AUDIO_BUFFER_SIZE)
-        
         // just start up the audio model here
-        audio.startMicrophoneProcessing(withFps: 10)
-        //audio.startProcesingAudioFileForPlayback()
-        audio.startProcessingSinewaveForPlayback(withFreq: 630.0)
+        audio.startMicrophoneProcessing()
+
         audio.play()
         
-        // run the loop for updating the graph peridocially
-        Timer.scheduledTimer(timeInterval: 0.05, target: self,
-            selector: #selector(self.updateGraph),
-            userInfo: nil,
-            repeats: true)
        
-    }
-    
-    
-    @objc
-    func updateGraph(){
-        self.graph?.updateGraph(
-            data: self.audio.fftData,
-            forKey: "fft"
-        )
-        
-        self.graph?.updateGraph(
-            data: self.audio.timeData,
-            forKey: "time"
-        )
-        
     }
     
     
