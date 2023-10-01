3207c4ff1b4ab1c4a9d7c9a28fb7509d2f4eb3e8
Trevor Dohm <trevordohm@gmail.com>
2023-09-30 18:25:55 -0700

    Microphone, FFT, Slider Working (Not playing inaudible tone though)

--- AudioLabSwift/AudioModel.swift
+++ AudioLabSwift/AudioModel.swift
@@ -12,36 +12,66 @@ import Accelerate
 class AudioModel {
     
     // MARK: Properties
+    
+    // These Properties Are For Interfacing With Novocaine, FFTHelper, Etc.
+    // User Can Access These Arrays Whenever Necessary And Plot Them
     private var BUFFER_SIZE:Int
-    // thse properties are for interfaceing with the API
-    // the user can access these arrays at any time and plot them if they like
     var timeData:[Float]
     var fftData:[Float]
-    
     var binnedFftData:[Float]
     
     // MARK: Public Methods
+    
+    // Anything Not Lazily Instantiated Should Be Allocated Here
     init(buffer_size:Int) {
         BUFFER_SIZE = buffer_size
-        // anything not lazily instatntiated should be allocated here
         timeData = Array.init(repeating: 0.0, count: BUFFER_SIZE)
         binnedFftData = Array.init(repeating: 0.0, count: 20)
         fftData = Array.init(repeating: 0.0, count: BUFFER_SIZE/2)
     }
     
     // public function for starting processing of microphone data
-    func startMicrophoneProcessing(withFps:Double){
+    func startMicrophoneProcessing(withFps:Double) {
         // setup the microphone to copy to circualr buffer
         if let manager = self.audioManager{
             manager.inputBlock = self.handleMicrophone
             
             // repeat this fps times per second using the timer class
             //   every time this is called, we update the arrays "timeData" and "fftData"
+            
+            // FIXME NEW TIMER https://www.swiftanytime.com/blog/ultimate-guide-on-timer-in-swift
+            
             Timer.scheduledTimer(withTimeInterval: 1.0/withFps, repeats: true) { _ in
                 self.runEveryInterval()
 //                self.copyBins(numBins: 20)
             }
             
+        }
+    }
+    
+    // Public Function For Starting Processing For Both Microphone, Speaker Data
+    func startDualProcessing(withFps:Double) {
+        
+        // Setup Microphone, Speaker For Copy To Circular Buffer
+        if let manager = self.audioManager{
+            manager.inputBlock = self.handleMicrophone
+            manager.outputBlock = self.handleSpeakerQueryWithSinusoids
+            
+            // repeat this fps times per second using the timer class
+            //   every time this is called, we update the arrays "timeData" and "fftData"
+            
+            Timer.scheduledTimer(withTimeInterval: 1.0/withFps, repeats: true) { _ in
+                self.runEveryInterval()
+//                self.copyBins(numBins: 20)
+            }
+            
+//            Timer.scheduledTimer(withTimeInterval: 1.0/5.0, repeats: true) { _ in
+//                // set to opposite
+//                self.pulseValue += 1
+//                if self.pulseValue > 5{
+//                    self.pulseValue = 0
+//                }
+//            }
         }
     }
     
@@ -155,5 +185,67 @@ class AudioModel {
         self.inputBuffer?.addNewFloatData(data, withNumSamples: Int64(numFrames))
     }
     
+    //    _     _     _     _     _     _     _     _     _     _
+    //   / \   / \   / \   / \   / \   / \   / \   / \   / \   /
+    //  /   \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/   \_/
+    var sineFrequency:Float = 0.0 { // frequency in Hz (changeable by user)
+        didSet{
+            
+        // if using swift for generating the sine wave: when changed, we need to update our increment
+//                    phaseIncrement = Float(2*Double.pi*Double(sineFrequency)/manager.samplingRate)
+            self.audioManager?.sineFrequency = sineFrequency
+        }
+    }
     
+    // Set the inaudible tone frequency
+    func setToneFrequency(_ frequency: Float) {
+        sineFrequency = frequency
+    }
+    
+    // SWIFT SINE WAVE
+    // everything below here is for the swift implementation
+    // this can be deleted when using the objective c implementation
+    private var phase:Float = 0.0
+    private var phaseIncrement:Float = 0.0
+    private var sineWaveRepeatMax:Float = Float(2*Double.pi)
+    
+    // Calculate and return the maximum decibel value from FFT data
+    func getMaxDecibels() -> Float {
+        // Use Accelerate framework to calculate the maximum value in decibels
+        var maxDecibels: Float = -Float.greatestFiniteMagnitude
+        vDSP_maxv(fftData, 1, &maxDecibels, vDSP_Length(fftData.count))
+        
+        // Convert from linear scale to decibels
+        let maxDecibelsInLinearScale = 20 * log10f(maxDecibels)
+        return maxDecibelsInLinearScale
+    }
+    
+    private func handleSpeakerQueryWithSinusoids(data:Optional<UnsafeMutablePointer<Float>>, numFrames:UInt32, numChannels: UInt32){
+        // while pretty fast, this loop is still not quite as fast as
+        // writing the code in c, so I placed a function in Novocaine to do it for you
+        // use setOutputBlockToPlaySineWave() in Novocaine
+        // EDIT: fixed in 2023
+        if let arrayData = data{
+            var i = 0
+            let chan = Int(numChannels)
+            let frame = Int(numFrames)
+            if chan==1{
+                while i<frame{
+                    arrayData[i] = sin(phase)
+                    phase += phaseIncrement
+                    if (phase >= sineWaveRepeatMax) { phase -= sineWaveRepeatMax }
+                    i+=1
+                }
+            }else if chan==2{
+                let len = frame*chan
+                while i<len{
+                    arrayData[i] = sin(phase)
+                    arrayData[i+1] = arrayData[i]
+                    phase += phaseIncrement
+                    if (phase >= sineWaveRepeatMax) { phase -= sineWaveRepeatMax }
+                    i+=2
+                }
+            }
+        }
+    }
 }
--- AudioLabSwift/Module2ViewController.swift
+++ AudioLabSwift/Module2ViewController.swift
@@ -8,21 +8,77 @@
 
 import UIKit
 
-class Module2ViewController: UIViewController {
+enum GestureType {
+    case towardsMicrophone
+    case awayFromMicrophone
+    case noGesture
+}
 
+class Module2ViewController: UIViewController {
+    
+    // Create AudioConstants (Structure With Any
+    // Constants Necessary To Run AudioModel)
+    struct AudioConstants {
+        static let AUDIO_BUFFER_SIZE = 1024 * 4
+    }
+    
+    // Create AudioModel Object With Specified Buffer Size
+    let audio = AudioModel(buffer_size: AudioConstants.AUDIO_BUFFER_SIZE)
+
+    // Outlets Defined On Storyboard - Hopefully Self-Explanatory!
     @IBOutlet weak var gesture_label: UILabel!
-    
     @IBOutlet weak var decibel_label: UILabel!
-    
     @IBOutlet weak var tone_slider_label: UILabel!
     @IBOutlet weak var tone_slider: UISlider!
 
+    // Runs When View Loads (With Super Method)
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Querying Microphone, Speaker From AudioModel With Preferred
+        // Calculations (Gestures, FFT, Doppler Shifts) Per Second
+        audio.startDualProcessing(withFps: 20)
 
-        // Do any additional setup after loading the view.
+        // Handle Audio
+        audio.play()
+        
+        // Set the initial tone frequency based on the slider value
+        audio.setToneFrequency(tone_slider.value)
+        
+        // Add an observer to update tone frequency when the slider is moved
+        tone_slider.addTarget(self, action: #selector(toneSliderValueChanged(_:)), for: .valueChanged)
+        
+        Timer.scheduledTimer(timeInterval: 1.0 / 20, target: self, selector: #selector(updateDecibelLabel), userInfo: nil, repeats: true)
     }
     
+    // Method to handle gesture detection based on Doppler shifts
+    private func detectGesture(usingFFT fftData: [Float]) -> GestureType {
+        // Implement your gesture detection logic based on Doppler shifts here
+        // You can analyze the FFT data to determine gestures towards, away, or no gesture
+        // This can involve comparing current and previous FFT data for Doppler shifts.
+        // For this example, let's assume a simple threshold-based approach.
+        let threshold: Float = 50.0 // Adjust this threshold as needed
+        let maxFFTValue = fftData.max() ?? 0.0
+        if maxFFTValue > threshold {
+            return .towardsMicrophone
+        } else {
+            return .awayFromMicrophone
+        }
+    }
+    
+    // Tone slider value changed
+    @objc func toneSliderValueChanged(_ sender: UISlider) {
+        audio.setToneFrequency(sender.value)
+        
+        // Update the slider label text with the current slider value
+        tone_slider_label.text = String(format: "Tone Slider: %.2f kHz", sender.value / 1000.0)
+    }
+        
+    // Update the label to display FFT magnitude in decibels
+    @objc func updateDecibelLabel() {
+        let maxDecibels = audio.getMaxDecibels()
+        decibel_label.text = "\(maxDecibels)"
+    }
 
     /*
     // MARK: - Navigation
