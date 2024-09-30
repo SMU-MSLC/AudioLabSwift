e8b23d8637eb31e4c03f1799084c2bdfdd29d413
Eric Larson <eclarson@smu.edu>
2024-09-30 08:26:39 -0500

    deletiing older versions of files mian branch is not really used in these examples

--- AudioLabSwift/AudioLabSwift-Bridging-Header.h
+++ AudioLabSwift/AudioLabSwift-Bridging-Header.h
@@ -2,7 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "Novocaine.h"
-#import "AudioFileReader.h"
-#import "CircularBuffer.h"
-#import "FFTHelper.h"
+
